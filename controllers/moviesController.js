d7ca258951c204c18fa91acc429c37c8f388f2a6
PinchaoRamiro <antonio.pinchao@utp.edu.co>
2024-07-04 19:56:56 -0500

    Terminado filtro de movies

--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -36,70 +36,56 @@ export const getMovieById = async (req, res) => {
 };
 
 export const listMovies = async (req, res) => {
-  const { language = "en-US", page = 1, genre, region, query } = req.query;
-  console.log(query);
+  const { language = 'en-US', page = 1, genre, region, query } = req.query;
 
-  if (query){
-    searchMoviesQuery(query);
-  } else{
-    try {
-      const params = {
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: `Bearer ${TMDB_API_TOKEN}`,
+    },
+  };
+
+  try {
+    if (query) {
+      // Construcción de la URL para la búsqueda
+      options.url = `${TMDB_API_URL}/search/movie`;
+      options.params = {
+        query,
+        include_adult: false,
+        language,
+        page,
+      };
+    } else {
+      // Construcción de la URL para la búsqueda con filtros
+      options.url = `${TMDB_API_URL}/discover/movie`;
+      options.params = {
         language,
         page,
         sort_by: 'popularity.desc',
         include_adult: false,
         include_video: false,
       };
-  
-      if (genre) params.with_genres = genre;
-      if (region) params.region = region;
-      if (query) params.query = query;
-  
-      const options = {
-        method: 'GET',
-        url: `${TMDB_API_URL}/discover/movie`,
-        headers: {
-          accept: 'application/json',
-          Authorization: `Bearer ${TMDB_API_TOKEN}`
-        },
-        params
-      };
-  
-      const response = await axios.request(options);
-      res.json(response.data);
-    } catch (error) {
-      if (error.response) {
-        res.status(error.response.status).send(error.response.data.status_message);
-      } else if (error.request) {
-        res.status(500).send('No response received from TMDb');
-      } else {
-        res.status(500).send('Internal server error');
-      }
+
+      if (genre) options.params.with_genres = genre;
+      if (region) options.params.region = region;
     }
-  }
 
-  
-};
-
-const searchMoviesQuery(query) => {
-  try{
-
-const options = {
-  method: 'GET',
-  url: 'https://api.themoviedb.org/3/search/movie?query=the%20matrix&include_adult=false',
-  headers: {
-    accept: 'application/json',
-    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiZjFjNzRhYTc3YjMxNWY2NjE1YzExNTY5MTFlZTE3OSIsIm5iZiI6MTcyMDEyNjg4OC42Mzg4NjIsInN1YiI6IjY2N2NjMGFmNjhiMjdmYzJlYmI5MWVhZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.6jdQDzAJ3-yGTPYexe_KvnBg1vESm7X-EYw--qygzB4'
-  }
-};
-
-axios
-  .request(options)
-  .then(function (response) {
+    // Solicitud a la API de TMDb
+    const response = await axios.request(options);
     res.json(response.data);
-  })
-  .catch(function (error) {
-    console.error(error);
-  });
+  } catch (error) {
+    handleError(error, res);
   }
-}
+};
+
+// Función para manejar errores
+const handleError = (error, res) => {
+  if (error.response) {
+    res.status(error.response.status).send(error.response.data.status_message);
+  } else if (error.request) {
+    res.status(500).send('No response received from TMDb');
+  } else {
+    res.status(500).send('Internal server error');
+  }
+};
