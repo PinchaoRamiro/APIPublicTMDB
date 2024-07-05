0f2b58fa086a68efd7143bab21f0d14743f85fcc
PinchaoRamiro <antonio.pinchao@utp.edu.co>
2024-07-05 01:26:50 -0500

    documentado

--- App.js
+++ App.js
@@ -1,19 +1,13 @@
-// Importaciones necesarias para configurar y ejecutar el servidor
-console.clear(); // Limpia la consola al iniciar el servidor
+// App.js
 import express from 'express'; // Importa el framework Express para crear el servidor
 import dotenv from 'dotenv'; // Importa dotenv para cargar variables de entorno desde un archivo .env
+import path from 'path'; // Importa path para manejar rutas de archivos
 
 // Importa los enrutadores personalizados para cada tipo de recurso
 import moviesRouter from './Routers/moviesRouter.js'; // Enrutador para las rutas relacionadas con películas
 import peopleRouter from './Routers/peopleRouter.js'; // Enrutador para las rutas relacionadas con personas
 import tvRouter from './Routers/tvRouter.js'; // Enrutador para las rutas relacionadas con series de televisión
 import trendingRouter from './Routers/trendingRouter.js'; // Enrutador para las rutas relacionadas con contenido trending
-
-//importa el index de la API
-import index from './index.js';
-
-// Middleware para servir archivos estáticos desde la carpeta 'public'
-app.use(express.static('public'));
 
 // Carga las variables de entorno desde un archivo .env si existe
 dotenv.config();
@@ -27,18 +21,16 @@ const port = process.env.PORT || 3000;
 // Middleware para interpretar el cuerpo de las solicitudes como objetos JSON
 app.use(express.json());
 
+// Middleware para servir archivos estáticos desde la carpeta 'public'
+app.use('/api', express.static(path.join(path.resolve(), 'public')));
+
 // Middleware para enrutar las solicitudes a los enrutadores específicos según la ruta base
 app.use('/api', moviesRouter); // Rutas relacionadas con películas
 app.use('/api', peopleRouter); // Rutas relacionadas con personas
 app.use('/api', tvRouter); // Rutas relacionadas con series de televisión
 app.use('/api', trendingRouter); // Rutas relacionadas con contenido trending
-app.use('/', index); // Ruta para el index de la API
 
 // Inicia el servidor y escucha en el puerto especificado
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`); // Muestra un mensaje en la consola cuando el servidor se inicia correctamente
 });
-
-
-
-
